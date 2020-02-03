b56c47ef44630d4c5c05c2d1ed892c14b69b6eae
Sarah <sarkwon89@gmail.com>
2020-02-03 15:01:01 -0800

    hw done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 8080;
 
 const app = express();
 
@@ -21,7 +21,9 @@ mongoose.connect("mongodb://localhost/budget", {
 });
 
 // routes here
+app.use(require("./routes/api.js"));
+
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`http://localhost:${PORT}`);
 });
